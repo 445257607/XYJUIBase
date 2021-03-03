c9330321055f983bbea7bd5dee886ac9137dfb0d
XiaoYingjun <445257607@qq.com>
2021-03-03 20:54:36 +0800

    create

---
+++ XYJUIBase/XYJUIBase.h
@@ -0,0 +1,18 @@
+//
+//  XYJUIBase.h
+//  XYJUIBase
+//
+//  Created by 肖迎军 on 2021/3/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XYJUIBase.
+FOUNDATION_EXPORT double XYJUIBaseVersionNumber;
+
+//! Project version string for XYJUIBase.
+FOUNDATION_EXPORT const unsigned char XYJUIBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYJUIBase/PublicHeader.h>
+
+
